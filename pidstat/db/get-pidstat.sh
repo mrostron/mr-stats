f85af2a48162dcb26aa6e9f5d9e24cc85ea2e908
Mark Rostron <rostron.mark@gmail.com>
2016-06-29 19:53:06 -0500

    minor change

--- pidstat/db/get-pidstat.sh
+++ pidstat/db/get-pidstat.sh
@@ -31,7 +31,7 @@ trap "echo trapped; kill -9 0" 1 2 3 15
 
 function get_list_of_files {
   typeset l_host=${1:?"list_of_files missing param 1 l_host"}
-  ssh ${l_host} "find ${PIDSTAT_SOURCE} -name pidstat.[0-9] -mtime -${DAYS_HIST} | xargs ls -Ggl --time-style='+%Y%m%d' | awk '{print \$4,\$5}'"
+  ssh ${l_host} "find ${PIDSTAT_SOURCE} -name pidstat.[0-9]* -mtime -${DAYS_HIST} | xargs ls -Ggl --time-style='+%Y%m%d' | awk '{print \$4,\$5}'"
 }
 
 
